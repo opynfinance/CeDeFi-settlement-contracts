7e1b3b5c2beb67134bb1ae70e69402b0aa3d7f4d
Haythem Sellami <sellamihaythem96@gmail.com>
2022-06-03 09:31:20 +0200

    init Settlement.sol

---
+++ src/Settlement.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.13;
+
+/// @title Settlement
+/// @author Haythem Sellami
+contract Settlement {
+    // CUSTOMS
+    //   error BadGm();
+    //   event GMEverybodyGM();
+
+
+    
+}
