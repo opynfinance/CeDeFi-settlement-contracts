b4aaf0e8f4a66857d3c82b9e1708936741072fee
Haythem Sellami <sellamihaythem96@gmail.com>
2022-06-20 11:49:11 +0200

    clean

--- deploy/settlement.sh
+++ deploy/settlement.sh
@@ -5,9 +5,12 @@ echo "Enter Your RPC URL(press RETURN for anvil deployment):"
 echo Example: https://eth-mainnet.alchemyapi.io/v2/XXXXXXXXXX 
 read -s rpc
 
+echo "Enter Your Etherscan API key(press RETURN for anvil deployment):"
+read -s etherscanKey
+
 if [ -z "$rpc" ]
 then
-  ETHERSCAN_API_KEY=PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8 forge create Settlement -i --rpc-url https://ropsten.infura.io/v3/9a1eacc6b18f436dab839c1713616fd1 --constructor-args "1" --verify
+  forge create Settlement -i --rpc-url http://localhost:8545 --constructor-args "1" --verify
 else
-  forge create Settlement -i --rpc-url $rpc --constructor-args "1" --verify
+  ETHERSCAN_API_KEY=etherscanKey forge create Settlement -i --rpc-url $rpc --constructor-args "1" --verify
 fi
